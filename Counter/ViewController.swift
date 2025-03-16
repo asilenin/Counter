d80eae24889656523d7a0cfaf8abab70f177e6d2
Anton Silenin <anton.silenin@gmail.com>
2025-03-16 23:40:39 +0300

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Anton Silenin on 16.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
